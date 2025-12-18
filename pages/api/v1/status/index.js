0f93bd768205a8122e73aaa7677a5a6a59b58f3b
fernando-cesxr <cesarfernandocarlos@gmail.com>
2025-12-18 19:38:07 -0300

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 import database from "infra/database.js";
 
-async function status(req, res) {
+async function status(request, response) {
   const updateAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
@@ -21,7 +21,7 @@ async function status(req, res) {
   const databaseOpennedConnetionsValue =
     databaseOpennedConnetionsResult.rows[0].count;
 
-  res.status(200).json({
+  response.status(200).json({
     updated_at: updateAt,
     dependencies: {
       database: {
